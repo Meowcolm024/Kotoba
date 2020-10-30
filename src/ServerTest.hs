e24ac795fae70e7b8c8beb12faf256ab46668937
Meowcolm024 <luoyicong1026@live.com>
2020-10-30 08:42:19 +0800

    get resp

--- src/ServerTest.hs
+++ src/ServerTest.hs
@@ -9,6 +9,7 @@ import Encrypt (newCipher)
 import Network.Simple.TCP
 import Network.Socket (SockAddr (SockAddrUnix))
 import SecureTcp
+import Data.Word (Word8)
 
 server :: IO ()
 server =
@@ -19,7 +20,7 @@ server =
 
 serverb :: IO ()
 serverb =
-  serve (Host "127.0.0.1") "8000" $ \(connectionSocket, remoteAddr) -> do
+  serve (Host "127.0.0.1") "1080" $ \(connectionSocket, remoteAddr) -> do
     x <- recv connectionSocket 64
     print (B.unpack <$> x)
 
@@ -30,17 +31,25 @@ serverb =
     case t of
       Nothing -> return ()
       Just t' -> do
-        let (tAddr, tPort) = getADDR t'
+        let (tAddr, tPort) = getADDR ((B.unpack t') !! 3) t'
         send connectionSocket (B.pack [0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
         print tAddr
+        h <- recv connectionSocket 1024
+        print h
+        -- send connectionSocket (B.pack [0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        let content = "<!doctype html><html><body>Hello World</body></html>"
+        send connectionSocket content
+        g <- recv connectionSocket 1024
+        print g
     -- TODO: http request
 
     putStrLn $ "TCP connection established from " ++ show remoteAddr
 
-getADDR :: ByteString -> (ByteString, ByteString)
-getADDR x = bimap B.pack B.pack $ go ([], s)
+getADDR :: Word8 -> ByteString -> (ByteString, ByteString)
+getADDR t x = bimap B.pack B.pack $ go ([], f t s)
   where
-    s = drop 4 $ B.unpack x
+    s =  B.unpack x
+    f x = if t == 0x03 then drop 5 else drop 4
     go (p, [a, b]) = (p, [a, b])
     go (a, (b : bs)) = go (a ++ [b], bs)
 
